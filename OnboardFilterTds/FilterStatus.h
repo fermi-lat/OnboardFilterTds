0c5707da2fdefa4e928862923db2f19d522a087c
usher <>
2008-05-17 02:31:21 +0000

    Checking changes between linux and windows

--- OnboardFilterTds/FilterStatus.h
+++ OnboardFilterTds/FilterStatus.h
@@ -17,7 +17,8 @@
 #include "EDS/EBF_dir.h"
 #include "EDS/EDR_tkr.h"
 #include "EFC/GFC_status.h"
-#include "EFC/TFC_prjDef.h"
+//#include "EFC/TFC_prjDef.h"
+#include "OnboardFilterTds/TFC_prjDef.h"
 #include "OnboardFilterTds/LogInfoDef.h"
 /**
  * @class FilterStatus
