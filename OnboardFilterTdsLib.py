5b0bc4fad80d16fd97aee74bf7aac1ad7c153f21
ecephas <>
2008-12-09 00:08:34 +0000

    Check in OnboardFilterTdsLib.py for OnboardFilterTds v0r10 (branch-tag, GlastReleasev15r49).

--- OnboardFilterTdsLib.py
+++ OnboardFilterTdsLib.py
@@ -1,7 +1,7 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/OnboardFilterTds/OnboardFilterTdsLib.py,v 1.1 2008/08/15 21:42:36 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['OnboardFilterTds'])
-
+    env.Tool('EventLib')
 def exists(env):
     return 1;
