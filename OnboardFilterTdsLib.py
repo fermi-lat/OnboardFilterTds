4fd65de8e6cdba15fdc0b6b30de94fa5f7a554bd
jrb <>
2012-04-26 21:48:47 +0000

    for slightly upgraded obf B3-0-0 (-gl1)

--- OnboardFilterTdsLib.py
+++ OnboardFilterTdsLib.py
@@ -1,7 +1,8 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/OnboardFilterTds/OnboardFilterTdsLib.py,v 1.1 2008/08/15 21:42:36 ecephas Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/OnboardFilterTds/OnboardFilterTdsLib.py,v 1.3 2009/01/23 00:21:00 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['OnboardFilterTds'])
     env.Tool('EventLib')
+    env.Tool('addLibrary', library=env['obfLibs'])
 def exists(env):
     return 1;
