21a0774e8f6d27f8a90562bd573bf738d065fb1d
jrb <>
2012-08-17 18:39:30 +0000

    patches for Windows no-install-headers

--- OnboardFilterTdsLib.py
+++ OnboardFilterTdsLib.py
@@ -1,7 +1,12 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/OnboardFilterTds/OnboardFilterTdsLib.py,v 1.3 2009/01/23 00:21:00 ecephas Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/OnboardFilterTds/OnboardFilterTdsLib.py,v 1.4 2012/04/26 21:48:45 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['OnboardFilterTds'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package='OnboardFilterTds')
+    if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+        env.Tool('findPkgPath', package = 'enums') 
+ 
     env.Tool('EventLib')
     env.Tool('addLibrary', library=env['obfLibs'])
 def exists(env):
